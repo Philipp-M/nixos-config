47dd9f472dc485d23443c19f45df9003c515d6af
Philipp Mildenberger <philipp@mildenberger.me>
2020-12-01 00:30:58 +0100

    added mpv super resolution shaders (FSRCNN)

--- nixos/home.nix
+++ nixos/home.nix
@@ -484,7 +484,7 @@
     enable = true;
     config = {
       x11-netwm = "yes"; # necessary for xmonads fullscreen
-      profile = "vdpau-high";
+      profile = "opengl-high";
       video-sync = "audio";
     };
     profiles = {
@@ -512,20 +512,23 @@
       opengl-high = {
         vo = "opengl";
         profile = "opengl-hq";
-        scale = "ewa_lanczossharp";
-        cscale = "ewa_lanczossharp";
         video-sync = "display-resample";
         interpolation = "yes";
         tscale = "oversample";
         ytdl-format = "bestvideo+bestaudio/best";
         x11-bypass-compositor = "yes";
         af = "acompressor";
+        glsl-shader = ""
+          + builtins.path { path = ../config/mpv/FSRCNN_x2_r2_32-0-2.glsl; };
+        display-fps = "60";
       };
       opengl-low = {
         vo = "opengl";
         profile = "opengl-hq";
         video-sync = "display-resample";
         ytdl-format = "bestvideo+bestaudio/best";
+        glsl-shader = ""
+          + builtins.path { path = ../config/mpv/FSRCNNX_x2_8-0-4-1.glsl; };
         x11-bypass-compositor = "yes";
         af = "acompressor";
       };
