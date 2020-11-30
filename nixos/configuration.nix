5aa4ac495f0d970b8bbf4e932ceba6a8981b1bac
Philipp Mildenberger <philipp@mildenberger.me>
2020-11-30 21:31:30 +0100

    [taffybar, nixos]: removed some now unnecessary fixes for taffybar (which lead to a broken state with current nixos-unstable)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -363,9 +363,6 @@
               mv all/gi-cairo-connector source
             '';
           }));
-        gi-dbusmenu = markUnbroken (hself.gi-dbusmenu_0_4_8);
-        gi-dbusmenugtk3 = markUnbroken (hself.gi-dbusmenugtk3_0_4_9);
-        gi-gdk = hself.gi-gdk_3_0_23;
         gi-gdkx11 = markUnbroken (overrideSrc hsuper.gi-gdkx11 {
           src = self.fetchurl {
             url =
@@ -374,10 +371,6 @@
           };
           version = "3.0.10";
         });
-        gi-gtk-hs = markUnbroken (hself.gi-gtk-hs_0_3_9);
-        gi-xlib = markUnbroken (hself.gi-xlib_2_0_9);
-        gtk-sni-tray = markUnbroken (hsuper.gtk-sni-tray);
-        gtk-strut = markUnbroken (hsuper.gtk-strut);
         taffybar = markUnbroken (appendPatch hsuper.taffybar (self.fetchpatch {
           url =
             "https://github.com/taffybar/taffybar/pull/494/commits/a7443324a549617f04d49c6dfeaf53f945dc2b98.patch";
