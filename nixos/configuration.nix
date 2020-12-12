7481e1fbed326e03dd492e5ed0720b0ee18d0748
Philipp Mildenberger <philipp@mildenberger.me>
2020-12-12 14:20:58 +0100

    removed extra haskell overlays for taffybar, as it's now working without any changes (apart from marking unbroken)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -363,47 +363,9 @@
 
       alacritty = super.callPackage ./alacritty.nix { };
 
-      haskellPackages = let
-        gi-cairo-render-src = self.fetchFromGitHub {
-          owner = "cohomology";
-          repo = "gi-cairo-render";
-          rev = "051de28ff092e0be0dc28612c6acb715a8bca846";
-          sha256 = "1v9kdycc91hh5s41n2i1dw2x6lxp9s1lnnb3qj6vy107qv8i4p6s";
-        };
-      in with self.haskell.lib;
-      super.haskellPackages.extend (hself: hsuper: {
-        gi-cairo-render = markUnbroken (overrideCabal (hsuper.gi-cairo-render)
-          (drv: {
-            src = gi-cairo-render-src;
-            editedCabalFile = null;
-            postUnpack = ''
-              mv source all
-              mv all/gi-cairo-render source
-            '';
-          }));
-        gi-cairo-connector = markUnbroken
-          (overrideCabal (hsuper.gi-cairo-connector) (drv: {
-            src = gi-cairo-render-src;
-            editedCabalFile = null;
-            postUnpack = ''
-              mv source all
-              mv all/gi-cairo-connector source
-            '';
-          }));
-        gi-gdkx11 = markUnbroken (overrideSrc hsuper.gi-gdkx11 {
-          src = self.fetchurl {
-            url =
-              "https://hackage.haskell.org/package/gi-gdkx11-3.0.10/gi-gdkx11-3.0.10.tar.gz";
-            sha256 = "0kfn4l5jqhllz514zw5cxf7181ybb5c11r680nwhr99b97yy0q9f";
-          };
-          version = "3.0.10";
-        });
-        taffybar = markUnbroken (appendPatch hsuper.taffybar (self.fetchpatch {
-          url =
-            "https://github.com/taffybar/taffybar/pull/494/commits/a7443324a549617f04d49c6dfeaf53f945dc2b98.patch";
-          sha256 = "0prskimfpapgncwc8si51lf0zxkkdghn33y3ysjky9a82dsbhcqi";
-        }));
-      });
+      haskellPackages = with self.haskell.lib;
+        super.haskellPackages.extend
+        (hself: hsuper: { taffybar = markUnbroken hsuper.taffybar; });
     })
   ];
 }
