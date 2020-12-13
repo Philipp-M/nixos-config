bba5297a5d41912b536e18828f9212d65c433751
Philipp Mildenberger <philipp@mildenberger.me>
2020-12-13 12:04:39 +0100

    added musnix kernel optimizations and a jackdbus service (which autostarts jack) for the machine zen

--- nixos/machines/zen/default.nix
+++ nixos/machines/zen/default.nix
@@ -7,9 +7,20 @@
     "/nix/var/nix/profiles/per-user/root/channels"
   ];
 
-  imports = [ ./hardware-configuration.nix ../../configuration.nix ];
+  imports = [
+    ./hardware-configuration.nix
+    ../../configuration.nix
+
+    (import "${(builtins.fetchTarball {
+      url =
+        "https://github.com/musnix/musnix/archive/6c3f31772c639f50f893c25fb4ee75bb0cd92c98.tar.gz";
+      sha256 = "07wwaxcilj2xi0j0a0kra1q65vb3ynddhxk72rvvnr9x144vqzvr";
+    })}")
+  ];
 
   boot.supportedFilesystems = [ "zfs" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.zenpower ];
+  boot.kernelModules = [ "snd-seq" "snd-rawmidi" ];
 
   networking.hostId = "80e43ffd";
   networking.hostName = "zen";
@@ -19,12 +30,33 @@
   networking.networkmanager.dns = "none";
 
   hardware.enableRedistributableFirmware = true;
+  hardware.pulseaudio.daemon.config = {
+    default-sample-format = "s32le";
+    default-sample-rate = 44100;
+    avoid-resampling = "yes";
+  };
 
   networking.interfaces.enp38s0.useDHCP = true;
   networking.interfaces.enp39s0.useDHCP = true;
   networking.interfaces.wlo1.useDHCP = true;
 
   virtualisation.docker.enableNvidia = true;
+
+  musnix = {
+    enable = true;
+
+    kernel.optimize = true;
+
+    alsaSeq.enable = false;
+
+    rtirq = {
+      # highList = "snd_hrtimer";
+      resetAll = 1;
+      prioLow = 0;
+      enable = true;
+      nameList = "rtc0 snd";
+    };
+  };
 
   services.xserver = {
     dpi = 110;
@@ -42,8 +74,60 @@
     videoDrivers = [ "nvidia" ];
   };
 
-  nix.maxJobs = lib.mkDefault 32;
+  users.users.philm.extraGroups = [ "jackaudio" ];
+
+  home-manager.users.philm.systemd.user.services.jackdbus = {
+    Unit = {
+      Description = "JACK 2 with pulseeffects support";
+      Requires = [ "dbus.socket" "pulseaudio.service" ];
+      After = [ "pulseaudio.service" ];
+    };
+
+    Install = { WantedBy = [ "graphical-session.target" ]; };
+
+    Service = with pkgs; {
+      Type = "dbus";
+      BusName = "org.jackaudio.service";
+
+      ExecStartPre = "-${killall}/bin/killall -9 jackdbus";
+
+      ExecStart = "${jack2}/bin/jackdbus auto";
+
+      # ExecStartPost = ''
+      ExecStartPost = [
+        "${coreutils}/bin/sleep 1"
+        "${jack2}/bin/jack_control ds alsa"
+        "${jack2}/bin/jack_control dps device hw:USB"
+        "${jack2}/bin/jack_control dps period 256"
+        "${jack2}/bin/jack_control dps nperiods 2"
+        "${jack2}/bin/jack_control dps rate 96000"
+        "${jack2}/bin/jack_control dps midi-driver alsarawmidi"
+        "${jack2}/bin/jack_control eps driver alsa"
+        "${jack2}/bin/jack_control eps realtime True"
+        "${jack2}/bin/jack_control eps realtime-priority 95"
+        "${jack2}/bin/jack_control start"
+        "-${pulseaudio}/bin/pacmd set-default-sink jack_out"
+        "-${pulseaudio}/bin/pacmd set-default-source jack_in"
+        "-${pulseaudio}/bin/pacmd suspend-sink jack_out 0"
+        "-${pulseaudio}/bin/pacmd suspend-source jack_in 0"
+      ];
+
+      ExecStop = "${jack2}/bin/jack_control exit";
+
+      ExecStopPost = [
+        "-${killall}/bin/pacmd set-default-sink alsa_output.usb-Focusrite_Scarlett_2i4_USB-00.analog-surround-40"
+        "-${killall}/bin/pacmd set-default-source alsa_input.usb-Focusrite_Scarlett_2i4_USB-00.analog-stereo"
+      ];
+
+      SuccessExitStatus = 0;
+      RemainAfterExit = "yes";
+    };
+  };
+
+  nix.maxJobs = lib.mkDefault 16;
   # High-DPI console
   console.font =
     lib.mkDefault "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+
+  environment.systemPackages = with pkgs; [ qjackctl libjack2 jack2 ];
 }
