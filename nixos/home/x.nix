450be805ce7f7f7991b373951e8559ac2e673800
Philipp Mildenberger <philipp@mildenberger.me>
2021-05-03 18:35:17 +0200

    modularized mpv config

--- nixos/home/x.nix
+++ nixos/home/x.nix
@@ -1,6 +1,7 @@
 # This contains most X.org desktop environment related stuff
 
 { pkgs, lib, config, ... }: {
+  imports = [ ./mpv ];
   home.keyboard.variant = "colemak";
   home.keyboard.layout = "us";
 
@@ -291,81 +292,5 @@
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs; [ obs-v4l2sink ];
-  };
-
-  programs.mpv = {
-    enable = true;
-    config = {
-      x11-netwm = "yes"; # necessary for xmonads fullscreen
-      profile = "opengl-high";
-      video-sync = "audio";
-    };
-    profiles = {
-      vdpau-high = {
-        vo = "vdpau";
-        profile = "opengl-hq";
-        scale = "ewa_lanczossharp";
-        cscale = "ewa_lanczossharp";
-        video-sync = "display-resample";
-        interpolation = "yes";
-        tscale = "oversample";
-        vf = "vdpaupp:deint=yes:deint-mode=temporal-spatial:hqscaling=1";
-        ytdl-format = "bestvideo+bestaudio/best";
-        x11-bypass-compositor = "yes";
-        af = "acompressor";
-      };
-      vdpau-low = {
-        vo = "vdpau";
-        profile = "opengl-hq";
-        video-sync = "display-resample";
-        ytdl-format = "bestvideo+bestaudio/best";
-        x11-bypass-compositor = "yes";
-        af = "acompressor";
-      };
-      opengl-high = {
-        vo = "opengl";
-        profile = "opengl-hq";
-        video-sync = "display-resample";
-        interpolation = "yes";
-        tscale = "oversample";
-        ytdl-format = "bestvideo+bestaudio/best";
-        x11-bypass-compositor = "yes";
-        af = "acompressor";
-        glsl-shader = ""
-          + builtins.path { path = ../../config/mpv/FSRCNN_x2_r2_32-0-2.glsl; };
-        display-fps = "60";
-      };
-      opengl-low = {
-        vo = "opengl";
-        profile = "opengl-hq";
-        video-sync = "display-resample";
-        ytdl-format = "bestvideo+bestaudio/best";
-        glsl-shader = ""
-          + builtins.path { path = ../../config/mpv/FSRCNNX_x2_8-0-4-1.glsl; };
-        x11-bypass-compositor = "yes";
-        af = "acompressor";
-      };
-      xv-high = {
-        vo = "xv";
-        profile = "opengl-hq";
-        scale = "ewa_lanczossharp";
-        cscale = "ewa_lanczossharp";
-        video-sync = "display-resample";
-        interpolation = "yes";
-        tscale = "oversample";
-        ytdl-format = "bestvideo+bestaudio/best";
-        x11-bypass-compositor = "yes";
-        af = "acompressor";
-      };
-      xv-low = {
-        vo = "xv";
-        profile = "opengl-hq";
-        video-sync = "display-resample";
-        ytdl-format = "bestvideo+bestaudio/best";
-        x11-bypass-compositor = "yes";
-        af = "acompressor";
-      };
-      fun = { vo = "tct"; };
-    };
   };
 }
