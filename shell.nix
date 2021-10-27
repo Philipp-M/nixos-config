4f17a43bc8419435c210e7d845591fbb664bf782
Philipp Mildenberger <philipp@mildenberger.me>
2021-10-27 02:07:27 +0200

    added devShell support

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> { }, inputs }:
+
+with pkgs;
+let nixBin =
+  writeShellScriptBin "nix" ''
+    ${nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
+  '';
+in
+mkShell {
+  buildInputs = [
+    git
+    nix-zsh-completions
+    git-crypt
+  ];
+  shellHook = ''
+    export FLAKE="$(pwd)"
+    export PATH="$FLAKE/bin:${nixBin}/bin:$PATH"
+  '';
+}
+
