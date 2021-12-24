4ae40c60754489d04c358eddd3ef0afda85e1dfa
Philipp Mildenberger <philipp@mildenberger.me>
2021-12-24 13:35:12 +0100

    shadow: add some notebook related stuff and refactor networking

--- machines/shadow/default.nix
+++ machines/shadow/default.nix
@@ -1,16 +1,29 @@
 { config, lib, pkgs, ... }: {
   imports = [ ./hardware-configuration.nix ../../configuration.nix ];
 
+  nix.maxJobs = lib.mkDefault 8;
+
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.requestEncryptionCredentials = false;
-
-  networking.hostId = "4ae8e232";
-  networking.hostName = "shadow";
-
   hardware.enableRedistributableFirmware = true;
 
-  networking.interfaces.enp3s0.useDHCP = true;
-  networking.interfaces.wlp4s0.useDHCP = true;
+  networking = {
+    hostId = "4ae8e232";
+    hostName = "shadow";
+    interfaces.enp3s0.useDHCP = true;
+    interfaces.wlp4s0.useDHCP = true;
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    # networkmanager.dns = "none";
+    networkmanager.enable = true;
+  };
 
-  nix.maxJobs = lib.mkDefault 8;
+
+  services.thermald.enable = true;
+
+  home-manager.users.philm.services.cbatticon = {
+    enable = true;
+    commandCriticalLevel = ''notify-send "battery critical!"'';
+  };
+  home-manager.users.philm.services.xembed-sni-proxy.enable = true;
+  home-manager.users.philm.programs.mpv.config.profile = lib.mkForce "gpu-low";
 }
