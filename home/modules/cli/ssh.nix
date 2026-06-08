d391916353d7ba895991c7f599ee33f7b2b87559
Philipp Mildenberger <philipp@mildenberger.me>
2026-06-08 15:41:36 +0200

    ssh: reduce alive interval because of client disconnects

--- home/modules/cli/ssh.nix
+++ home/modules/cli/ssh.nix
@@ -4,9 +4,7 @@
 
   config = lib.mkIf config.modules.cli.ssh.enable {
 
-    home.file.".ssh/config" = {
-      target = ".ssh/config_source";
-    };
+    home.file.".ssh/config".target = ".ssh/config_source";
 
     home.activation.installSshConfig =
       lib.hm.dag.entryAfter [ "linkGeneration" ] ''
@@ -18,8 +16,9 @@
       enableDefaultConfig = false;
       # TODO whitelist this for only a selected number of hosts
       settings."*" = {
-        ServerAliveInterval = 240;
-        ServerAliveCountMax = 3;
+        ServerAliveInterval = 30;
+        ServerAliveCountMax = 4;
+        TCPKeepAlive = "yes";
         ForwardX11 = "yes";
         ForwardX11Trusted = "yes";
         AddKeysToAgent = "yes";
